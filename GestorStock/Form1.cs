fbe08550c8b8e9e1dbe320f0c6a34800b4b8749e
Juan Prato <pratojuanmanuel2@gmail.com>
2024-09-09 18:58:21 -0300

    gitignore

--- GestorStock/Form1.cs
+++ GestorStock/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,17 @@ namespace GestorStock
 {
     public partial class Form1 : Form
     {
+
+        SqlConnection sQLConnection = null;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
