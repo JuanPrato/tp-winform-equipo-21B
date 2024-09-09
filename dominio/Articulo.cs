57279a74be2eb7e86dcd0084feecf31b50db84b6
mauricio.villalba@alumnos.frgp.utn.edu.ar <mauricio.villalba@alumnos.frgp.utn.edu.ar>
2024-09-09 19:49:05 -0300

    .

---
+++ dominio/Articulo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    public class Articulo
+    {
+        public string Codigo { get; set; }
+        [DisplayName("Nombre")]
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public Marca IdMarca { get; set; }
+        public Categoria IdCategoria { get; set; }
+        //public Urlimagen list<Urlimagen>
+        public decimal Precio { get; set; }
+    }
+}
